630e28f12c62e21011f59ee4466233d637a35cf6
Alex Gregory <axander.gregory@gmail.com>
2023-07-08 21:20:43 +0100

    terraform resource naming module

---
+++ terraform/modules/aws/resource-naming/outputs.tf
@@ -0,0 +1,27 @@
+output "iam_role_prefix" {
+  value = local.iam_role_prefix
+}
+
+output "iam_policy_prefix" {
+  value = local.iam_policy_prefix
+}
+
+output "parameter_store_prefix" {
+  value = local.parameter_store_prefix
+}
+
+output "secrets_manager_prefix" {
+  value = local.secrets_manager_prefix
+}
+
+output "security_group_prefix" {
+  value = local.security_group_prefix
+}
+
+output "s3_bucket_prefix" {
+  value = local.s3_bucket_prefix
+}
+
+output "vpc_prefix" {
+  value = local.vpc_prefix
+}
